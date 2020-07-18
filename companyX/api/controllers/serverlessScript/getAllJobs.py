c34ce5f0b745d2ebfafa4d2ac53d8385380e0d33
Nawaphan Chayopathum <chayopat@sheridanc.on.ca>
2020-07-18 20:06:07 -0300

    add serverless script for getAllJobs, update, and delete for Jobs table

---
+++ companyX/api/controllers/serverlessScript/getAllJobs.py
@@ -0,0 +1,53 @@
+import json
+import pymysql
+
+print('Loading function')
+
+#Configuration Values
+endpoint = 'tutorial-db-instance.cy6kirdolrfe.us-east-1.rds.amazonaws.com'
+username = 'tutorial_user'
+password = '052020Aws-Educate'
+database_name = 'cloud'
+
+
+
+def lambda_handler(event, context):
+	#1. query database
+	jobsResponse= query_database()
+
+
+	#2. Construct http response object
+	responseObject = {}
+	responseObject['statusCode'] = 200
+	responseObject['headers'] = {}
+	responseObject['headers']['Content-Type'] = 'application/json'
+	responseObject['body'] = json.dumps(jobsResponse)
+
+	#4. Return the response object
+	print("Done!")
+	return responseObject
+
+
+def query_database():
+	#Connection
+	connection = pymysql.connect(endpoint, user=username, passwd=password, db=database_name, charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+	result= []
+	try:
+	    with connection.cursor() as cursor:
+	        # Read a single record
+	        sql = "SELECT * FROM jobs"
+	        cursor.execute(sql)
+	        result = cursor.fetchall()
+	        print(result)
+	        print(type(result))
+	        # if(result is None):
+	        # 	print("There are no results")
+	        # else:
+		       #  print(result[0])
+		       #  print(result[1])
+		       #  newFreq= result[1] + addFreq
+	finally:
+	    connection.close()
+	
+	return result
---
+++ companyX/api/controllers/serverlessScript/updateJob.py
@@ -0,0 +1,68 @@
+import json
+import pymysql
+
+print('Loading function')
+
+#Configuration Values
+endpoint = 'tutorial-db-instance.cy6kirdolrfe.us-east-1.rds.amazonaws.com'
+username = 'tutorial_user'
+password = '052020Aws-Educate'
+database_name = 'cloud'
+
+
+
+def lambda_handler(event, context):
+	#1. query database
+	print(event)
+	body= json.loads(event['body'])
+	jobsResponse= query_database(body)
+
+
+	#2. Construct http response object
+	responseObject = {}
+	responseObject['statusCode'] = 200
+	responseObject['headers'] = {}
+	responseObject['headers']['Content-Type'] = 'application/json'
+	responseObject['body'] = json.dumps(jobsResponse)
+
+	#4. Return the response object
+	print("Done!")
+	return responseObject
+
+
+def query_database(body):
+	#Connection
+	connection = pymysql.connect(endpoint, user=username, passwd=password, db=database_name, charset='utf8mb4',
+	                         cursorclass=pymysql.cursors.DictCursor)
+	# result= {"status": 'unsuccess'}
+	
+	partId= body['partId']
+	jobName= body['jobName'] 
+	qty= body['qty']
+	
+	try:
+		with connection.cursor() as cursor:
+			# Read a single record
+			sql = "SELECT * FROM jobs WHERE jobName = %s AND partId = %s"
+			cursor.execute(sql, (jobName, int(partId)) )
+			result = cursor.fetchone()
+			
+			if(result is None):
+				result= {"status": 'unsuccess', "code": '400', "message": "jobName: " + jobName + " with " + "partId: " + str(partId) + " do not exist, can't update data."}
+			else:
+				with connection.cursor() as cursor:
+					sql = "UPDATE jobs SET qty = %s WHERE jobName = %s AND partId = %s"
+					cursor.execute(sql, (qty, jobName, int(partId) ) )
+					connection.commit()
+					result= {"status": 'success'}
+				
+	except pymysql.Error:
+	    raise RuntimeError(
+	"Cannot connect to database. "
+	"Create a group of connection parameters under the heading "
+	"[pandas] in your system's mysql default file, "
+	"typically located at ~/.my.cnf or /etc/.my.cnf.")
+	finally:
+	    connection.close()
+	
+	return result
