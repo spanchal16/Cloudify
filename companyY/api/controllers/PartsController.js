d0c9582bd680ba1de5c1b34e21779cd9ad78b626
Dhruv Dalwadi <dh844999@dal.ca>
2020-07-27 11:51:37 -0300

    Company Y - Fixed Bug

--- companyY/api/controllers/PartsController.js
+++ companyY/api/controllers/PartsController.js
@@ -39,65 +39,65 @@ module.exports = {
       });
   },
 
-    //To fetch existing part
-    editpart: async function (req, res) {
-      await axios({
-        method: "get",
-        url: `https://c8yyanelhf.execute-api.us-east-1.amazonaws.com/production/getpart?partid=${req.params.partid}`,
+  //To fetch existing part
+  editpart: async function (req, res) {
+    await axios({
+      method: "get",
+      url: `https://c8yyanelhf.execute-api.us-east-1.amazonaws.com/production/getpart?partid=${req.params.partid}`,
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          return res.view("pages/editpart", { parts: response.data[0] });
+        } else {
+          return res.send(500, { error: response.data });
+        }
       })
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            return res.view("pages/editpart", { parts: response.data[0] });
-          } else {
-            return res.send(500, { error: response.data });
-          }
-        })
-        .catch((err) => {
-          return res.send(500, { error: `Something Went Wrong!\n${err}` });
-        });
-    },
-  
-    //To update part
-    update: async function (req, res) {
-      await axios({
-        method: "post",
-        url: `https://c8yyanelhf.execute-api.us-east-1.amazonaws.com/production/editpart?partid=${req.body.txtpartid}&partname=${req.body.txtpartname}&qoh=${req.body.txtqoh}`,
+      .catch((err) => {
+        return res.send(500, { error: `Something Went Wrong!\n${err}` });
+      });
+  },
+
+  //To update part
+  update: async function (req, res) {
+    await axios({
+      method: "post",
+      url: `https://c8yyanelhf.execute-api.us-east-1.amazonaws.com/production/editpart?partid=${req.body.txtpartid}&partname=${req.body.txtpartname}&qoh=${req.body.txtqoh}`,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          res.redirect("/");
+        } else {
+          return res.send(500, { error: response.data });
+        }
       })
-        .then((response) => {
-          if (response.status === 200) {
-            res.redirect("/");
+      .catch((err) => {
+        return res.send(500, { error: `Something Went Wrong!\n${err}` });
+      });
+  },
+
+  //To search part
+  search: async function (req, res) {
+    await axios({
+      method: "get",
+      url: `https://c8yyanelhf.execute-api.us-east-1.amazonaws.com/production/getpart?partid=${req.body.txtpartid}`,
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          if (response.data.length !== 0) {
+            return res.view("pages/searchpart", { parts: response.data[0] });
           } else {
-            return res.send(500, { error: response.data });
+            return res.view("pages/searchpart", { parts: null });
           }
-        })
-        .catch((err) => {
-          return res.send(500, { error: `Something Went Wrong!\n${err}` });
-        });
-    },
-  
-    //To search part
-    search: async function (req, res) {
-      await axios({
-        method: "get",
-        url: `https://c8yyanelhf.execute-api.us-east-1.amazonaws.com/production/getpart?partid=${req.body.txtpartid}`,
+        } else {
+          return res.send(500, { error: response.data });
+        }
       })
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            if (response.data.length !== 0) {
-              return res.view("pages/searchpart", { parts: response.data[0] });
-            } else {
-              return res.view("pages/searchpart", { parts: null });
-            }
-          } else {
-            return res.send(500, { error: response.data });
-          }
-        })
-        .catch((err) => {
-          return res.send(500, { error: `Something Went Wrong!\n${err}` });
-        });
-    },
+      .catch((err) => {
+        return res.send(500, { error: `Something Went Wrong!\n${err}` });
+      });
+  },
 
   //To list all parts using (API)
   listAllParts: async function (req, res) {
@@ -148,7 +148,7 @@ module.exports = {
       })
         .then((response) => {
           if (response.status === 200) {
-            res.redirect("/");
+            return res.ok();
           } else {
             return res.send(500, { error: response.data });
           }
@@ -158,7 +158,6 @@ module.exports = {
         });
     }
   },
-
 
   //Search particular part using (API)
   searchPart: async function (req, res) {
